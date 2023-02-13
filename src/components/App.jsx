4f8cfc0eb23dddd6c442118435bc923b6c320397
VladimirGvv <vovagvv69@gmail.com>
2023-02-13 16:45:42 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { PhoneForm } from './PhoneForm/PhoneForm';
+import { Filter } from "./Filter/Filter";
+import { Contacts } from "./Contacts/Contacts";
+import styles from './App.module.scss';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
+
+export function App() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  
+    return (
+      <div className={styles.phone_form}>
+        <h1>Phonebook</h1>
+        <PhoneForm />
+        <h2>Contacts</h2>
+        <Filter />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <Contacts />
+      </div>
+    );
 };
